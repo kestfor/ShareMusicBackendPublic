4540977e4fe1b93af059f7f8499c03190b93ce68
Ilya <anzhinilya2@gmail.com>
2024-09-24 13:07:06 +0700

    bugfix

--- backend/app.py
+++ backend/app.py
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     print("http://127.0.0.1:8000/")
     print("http://127.0.0.1:8000/login/")
     print("http://127.0.0.1:8000/docs")
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=80)
